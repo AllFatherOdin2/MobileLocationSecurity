dfd931240b50deaf51045574aeb00b8df23aad82
AllFatherOdin2 <johnboz@comcast.net>
2016-04-19 14:00:03 -0400

    added schema file

---
+++ MobileLocationSecurity/app/src/main/java/com/jtbosworth/mobilelocationsecurity/database/FileDBSchema.java
@@ -0,0 +1,20 @@
+package com.jtbosworth.mobilelocationsecurity.database;
+
+/**
+ * Created by Helios on 4/19/2016.
+ */
+
+public class FileDBSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "files";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String LOCATION = "location";
+            public static final String CONTENT = "content";
+            public static final String FILE_TYPE = "fileType";
+        }
+    }
+}
