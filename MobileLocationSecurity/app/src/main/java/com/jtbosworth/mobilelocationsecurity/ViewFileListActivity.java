d7b60f37ec421f22045986a790a52f6cc3782a1d
AllFatherOdin2 <johnboz@comcast.net>
2016-04-21 15:30:06 -0400

    got the scroll view set up and using the full display

--- MobileLocationSecurity/app/src/main/java/com/jtbosworth/mobilelocationsecurity/ViewFileListActivity.java
+++ MobileLocationSecurity/app/src/main/java/com/jtbosworth/mobilelocationsecurity/ViewFileListActivity.java
@@ -1,17 +1,21 @@
 package com.jtbosworth.mobilelocationsecurity;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ViewFileListActivity extends AppCompatActivity implements LocationListener {
+public class ViewFileListActivity extends AppCompatActivity implements LocationListener, LocationBasedNotification.OnFragmentInteractionListener {
     protected LocationManager locationManager;
     protected Context context;
     private final int LLD_RANGE = 10;
@@ -27,6 +31,46 @@ public class ViewFileListActivity extends AppCompatActivity implements LocationL
         } catch (SecurityException e) {
             Log.e("ViewFileListActivity", "Failed to request location update - Permissions not found");
         }
+
+        Fragment fragment = new LocationBasedNotification();
+        Fragment fragment1 = new LocationBasedNotification();
+        Fragment fragment2 = new LocationBasedNotification();
+        Fragment fragment3 = new LocationBasedNotification();
+        Fragment fragment4 = new LocationBasedNotification();
+        Fragment fragment5 = new LocationBasedNotification();
+        Fragment fragment6 = new LocationBasedNotification();
+        Fragment fragment7 = new LocationBasedNotification();
+        Fragment fragment8 = new LocationBasedNotification();
+        Fragment fragment9 = new LocationBasedNotification();
+        Fragment fragment10 = new LocationBasedNotification();
+        Fragment fragment11 = new LocationBasedNotification();
+        Fragment fragment12 = new LocationBasedNotification();
+        Fragment fragment13 = new LocationBasedNotification();
+        Fragment fragment14 = new LocationBasedNotification();
+        Fragment fragment15 = new LocationBasedNotification();
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+
+        fragmentTransaction.add(R.id.locationBasedNotificationsScrollView, fragment);
+        fragmentTransaction.add(R.id.locationBasedNotificationsScrollView, fragment1);
+        fragmentTransaction.add(R.id.locationBasedNotificationsScrollView, fragment2);
+        fragmentTransaction.add(R.id.locationBasedNotificationsScrollView, fragment3);
+        fragmentTransaction.add(R.id.locationBasedNotificationsScrollView, fragment4);
+        fragmentTransaction.add(R.id.unlockedDocumentScrollView, fragment5);
+        fragmentTransaction.add(R.id.unlockedDocumentScrollView, fragment6);
+        fragmentTransaction.add(R.id.unlockedDocumentScrollView, fragment7);
+        fragmentTransaction.add(R.id.unlockedDocumentScrollView, fragment8);
+        fragmentTransaction.add(R.id.unlockedDocumentScrollView, fragment9);
+        fragmentTransaction.add(R.id.unlockedDocumentScrollView, fragment10);
+        fragmentTransaction.add(R.id.locationLockedDocumentScrollView, fragment11);
+        fragmentTransaction.add(R.id.locationLockedDocumentScrollView, fragment12);
+        fragmentTransaction.add(R.id.locationLockedDocumentScrollView, fragment13);
+        fragmentTransaction.add(R.id.locationLockedDocumentScrollView, fragment14);
+        fragmentTransaction.add(R.id.locationLockedDocumentScrollView, fragment15);
+
+        fragmentTransaction.commit();
+
     }
 
 
@@ -79,5 +123,10 @@ public class ViewFileListActivity extends AppCompatActivity implements LocationL
     @Override
     public void onProviderDisabled(String provider) {
         Log.i("LocationListener","Provider Disabled");
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Log.i("FragmentInteraction", "Reached onFragmentInteraction in ViewFileListActivity.java");
     }
 }
